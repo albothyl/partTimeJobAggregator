9aaa0a3eca90300c74f0b2bbff0a17a1118d43b2
알보칠(전형원) <jjhwqqq@coupang.com>
2015-06-07 00:35:36 +0900

    flyway initialize 권한 수정.

--- src/main/resources/db/migration/sql/initialize.sql
+++ src/main/resources/db/migration/sql/initialize.sql
@@ -1,14 +1,13 @@
 CREATE DATABASE partTimeJob DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
-CREATE USER 'wowman'@'%' IDENTIFIED BY '1q2w3e!';
 
-CREATE DATABASE sonar DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
-CREATE USER 'sonar'@'%' IDENTIFIED BY 'sonar';
+CREATE USER 'wowman'@'%' IDENTIFIED BY '1q2w3e!';
+CREATE USER 'wowman'@'localhost' IDENTIFIED BY '1q2w3e!';
 
 USE mysql;
 
 -- DB 별 권한 부여
-INSERT INTO db (HOST,Db,USER,Select_priv,Insert_priv,Update_priv,Delete_priv,Create_priv,Drop_priv,Index_priv, Alter_priv) VALUES('%','partTimeJob','wowman','Y','Y','Y','Y','Y','Y','Y','Y');
-GRANT ALL PRIVILEGES ON *.* TO 'sonar'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'wowman'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'wowman'@'localhost' WITH GRANT OPTION;
 
 -- 마무리.
 FLUSH PRIVILEGES;
