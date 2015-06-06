5c54d75e46bc9871ac28377e8cf59750893e1e23
알보칠(전형원) <jjhwqqq@coupang.com>
2015-06-07 01:52:35 +0900

    flyway create table 쿼리 수정

--- src/main/resources/db/migration/sql/V1__CreateTable_MEMBER.sql
+++ src/main/resources/db/migration/sql/V1__CreateTable_MEMBER.sql
@@ -1,16 +1,15 @@
 USE parttimejob;
 
 CREATE TABLE `member` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '회원 시퀀스 ID',
-  `email` varchar(60) COLLATE utf8_bin NOT NULL COMMENT '회원 email계정',
-  `password` varchar(60) COLLATE utf8_bin NOT NULL COMMENT '회원 비밀번호',
-  `name` varchar(20) COLLATE utf8_bin NOT NULL COMMENT '회원 이름',
-  `nickName` varchar(20) COLLATE utf8_bin NOT NULL COMMENT '회원 별명',
-  `grade` varchar(10) COLLATE utf8_bin NOT NULL COMMENT '회원 등급',
-  `updatedAt` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '회원 정보 수정일시',
-  `registedAt` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '회원 등록일시',
+  `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '?? ??? ID',
+  `email` varchar(60) COLLATE utf8_bin NOT NULL COMMENT '?? email??',
+  `password` varchar(60) COLLATE utf8_bin NOT NULL COMMENT '?? ????',
+  `name` varchar(20) COLLATE utf8_bin NOT NULL COMMENT '?? ??',
+  `nickName` varchar(20) COLLATE utf8_bin NOT NULL COMMENT '?? ??',
+  `grade` varchar(10) COLLATE utf8_bin NOT NULL COMMENT '?? ??',
+  `updatedAt` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '?? ?? ????',
+  `registedAt` datetime DEFAULT CURRENT_TIMESTAMP COMMENT '?? ????',
   PRIMARY KEY (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`),
-  UNIQUE KEY `nick_name_UNIQUE` (`nick_name`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
+  UNIQUE KEY `nick_name_UNIQUE` (`nickName`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
